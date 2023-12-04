1d9979fd43e3d360066a207b4e37304ffe1ad8cc
Dariel <gomez.b.dariel@gmail.com>
2023-12-04 17:45:11 -0600

    Pantalla protegida

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,10 +13,11 @@ export class RegisterComponent {
     email: ['test4@gmail.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   registro() {
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+
+    this.router.navigateByUrl('/dashboard');
   }
 }
