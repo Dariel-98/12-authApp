a2d22fe94916eda5d2abd60ba9404baef00ceba4
Dariel <gomez.b.dariel@gmail.com>
2023-12-05 07:05:19 -0600

    Registro de usuarios

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,11 +17,26 @@ export class RegisterComponent {
     email: ['test4@gmail.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   registro() {
     console.log(this.miFormulario.value);
 
-    this.router.navigateByUrl('/dashboard');
+    // Crear nuevo usuario
+
+    const { name, email, password } = this.miFormulario.value;
+
+    this.authService.registro(name, email, password).subscribe((ok) => {
+      if (ok === true) {
+        this.router.navigateByUrl('/dashboard');
+      } else {
+        Swal.fire('Error', ok, 'error');
+      }
+    });
+    // this.router.navigateByUrl('/dashboard');
   }
 }
